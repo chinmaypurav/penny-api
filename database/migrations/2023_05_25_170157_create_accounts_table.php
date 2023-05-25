2e3fd93331a67b5a667b882a4c1edc4290eb4188
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2023-05-25 23:20:31 +0530

    missing FK added in accounts table

--- database/migrations/2023_05_25_170157_create_accounts_table.php
+++ database/migrations/2023_05_25_170157_create_accounts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     {
         Schema::create('accounts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->string('name');
             $table->enum('accounty_type', AccountType::all())->default('SAVINGS');
             $table->decimal('balance', 16)->default(0);
