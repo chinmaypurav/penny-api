2024147a803f3a4172db8dc26d27c664611512f1
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2023-05-26 02:07:26 +0530

    wip

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -14,7 +14,6 @@ class Account extends Model
     protected $fillable = ['name', 'account_type', 'balance'];
 
     protected $casts = [
-        'account_type' => AccountType::class,
         'balance' => 'double',
     ];
 
