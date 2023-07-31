7c1d6f1142e912b2413f0e6caa1f693b764898bc
Chinmay Purav <70144052+chinmaypurav@users.noreply.github.com>
2023-08-01 01:36:39 +0530

    TransferService index date filter added

--- app/Services/TransferService.php
+++ app/Services/TransferService.php
@@ -4,14 +4,25 @@ namespace App\Services;
 
 use App\Models\Transfer;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
 class TransferService
 {
     public function index(User $user, array $params): Collection
     {
-        return $user->transfers()->get();
+        return $user->transfers()
+            ->when($params,
+                fn (Builder $q) => $q->when($from = Arr::get($params, 'date_from'),
+                    fn (Builder $q) => $q->whereDate('transacted_at', '>=', $from)
+                )->when($to = Arr::get($params, 'date_to'),
+                    fn (Builder $q) => $q->whereDate('transacted_at', '<=', $to)
+                )
+            )
+            ->with(['debtorAccount', 'creditorAccount'])
+            ->get();
     }
 
     public function store(User $user, array $input): Transfer
