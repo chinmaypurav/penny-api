05ac719c886c56db174acd2e96c71b07a2cbd308
Chinmay Purav <chinpurav@gmail.com>
2024-01-21 16:18:53 +0530

    Staging Seeder added with admin account

---
+++ config/penny.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'admin' => [
+        'default_email' => env('ADMIN_DEFAULT_EMAIL', ''),
+        'default_password' => env('ADMIN_DEFAULT_PASSWORD', ''),
+    ],
+];
---
+++ database/seeders/StagingSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class StagingSeeder extends Seeder
+{
+    public function run(): void
+    {
+        if (!User::count()) {
+            return;
+        }
+
+        User::create([
+            'name' => 'Admin',
+            'email' => config('penny.admin.default_email'),
+            'password' => config('penny.admin.default_password'),
+        ]);
+    }
+}
