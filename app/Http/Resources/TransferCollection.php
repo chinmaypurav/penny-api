f432fe8cd025d657884af2bf6087dca0520295f5
Chinmay Purav <chinpurav@gmail.com>
2024-02-24 00:52:30 +0530

    doc block

--- app/Http/Resources/TransferCollection.php
+++ app/Http/Resources/TransferCollection.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Transfer;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-/** @see Transfer */
+/** @see \App\Models\Transfer */
 class TransferCollection extends ResourceCollection
 {
     public static $wrap = 'transfers';
