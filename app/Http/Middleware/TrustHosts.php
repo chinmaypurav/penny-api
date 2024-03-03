73176904afb50008747358a870be8cb81a1087e7
Chinmay Purav <chinpurav@gmail.com>
2024-03-03 17:56:30 +0530

    wip

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -14,7 +14,6 @@ class TrustHosts extends Middleware
     public function hosts(): array
     {
         return [
-            'penny-nuxt.vercel.app',
             $this->allSubdomainsOfApplicationUrl(),
         ];
     }
