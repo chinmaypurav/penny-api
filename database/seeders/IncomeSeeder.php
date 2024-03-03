63082acc7ac27065de5b98015f73b05e1bfa2393
Chinmay Purav <chinpurav@gmail.com>
2024-03-03 17:56:00 +0530

    seeder fix

--- database/seeders/IncomeSeeder.php
+++ database/seeders/IncomeSeeder.php
@@ -7,9 +7,6 @@ use Illuminate\Database\Seeder;
 
 class IncomeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         Income::factory(5)->create();
