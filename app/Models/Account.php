08345ff86da1a130c529a5696c95296a3cb725d8
Chinmay Purav <chinpurav@gmail.com>
2024-03-04 23:07:41 +0530

    AccountType cast added to Account.php model

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\AccountType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -14,6 +15,7 @@ class Account extends Model
     protected $fillable = ['name', 'account_type', 'balance'];
 
     protected $casts = [
+        'account_type' => AccountType::class,
         'balance' => 'double',
     ];
 
