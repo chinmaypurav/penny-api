d7825b4569e211271f19a2725bc234a0aed7d457
Chinmay Purav <chinpurav@gmail.com>
2024-03-07 01:12:16 +0530

    test renamed to it

--- tests/Feature/Http/Controllers/ExpenseControllerTest.php
+++ tests/Feature/Http/Controllers/ExpenseControllerTest.php
@@ -6,8 +6,11 @@ use App\Models\Category;
 use App\Models\Expense;
 use App\Models\User;
 use App\Services\ExpenseService;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-uses(\Illuminate\Foundation\Testing\DatabaseMigrations::class);
+use function Pest\Laravel\actingAs;
+
+uses(DatabaseMigrations::class);
 
 beforeEach(function () {
     $this->service = app(ExpenseService::class);
@@ -42,7 +45,7 @@ it('stores expense to expenses table and adjusts account', function (AccountType
         'amount' => 5000,
     ];
 
-    $this->actingAs($this->user)
+    actingAs($this->user)
         ->postJson('api/expenses', $payload)
         ->assertCreated();
 
@@ -56,7 +59,7 @@ it('stores expense to expenses table and adjusts account', function (AccountType
     ]);
 })->with('getExpenseDataset');
 
-test('deletes expense from expenses table and adjusts account', function (AccountType $accountType) {
+it('deletes expense from expenses table and adjusts account', function (AccountType $accountType) {
     $account = Account::factory()->create([
         'user_id' => $this->user,
         'account_type' => $accountType,
@@ -78,7 +81,7 @@ test('deletes expense from expenses table and adjusts account', function (Accoun
         'category_id' => $category,
     ]);
 
-    $this->actingAs($this->user)
+    actingAs($this->user)
         ->deleteJson('api/expenses/'.$expense->id, $payload)
         ->assertNoContent();
 
