bb5d789e7fa2bd343dc0a040e83e5c4e5a81d5d4
Chinmay Purav <chinpurav@gmail.com>
2024-03-17 02:40:47 +0530

    TransactionControllerTest.php added

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -7,15 +7,15 @@ use App\Models\Expense;
 use App\Models\Income;
 use App\Services\ExpenseService;
 use App\Services\IncomeService;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TransactionController extends Controller
 {
-    public function __invoke(IncomeService $incomeService, ExpenseService $expenseService, Request $request)
+    public function __invoke(IncomeService $incomeService, ExpenseService $expenseService)
     {
         $transactions = collect()
-            ->merge($incomeService->index($request->user()))
-            ->merge($expenseService->index($request->user()))
+            ->merge($incomeService->index(Auth::id()))
+            ->merge($expenseService->index(Auth::id()))
             ->sort(fn (Income|Expense $model) => $model->transacted_at->getTimestamp());
 
         return TransactionCollection::make($transactions);
---
+++ tests/Feature/Http/Controllers/TransactionControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Expense;
+use App\Models\Income;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+use function Pest\Laravel\actingAs;
+
+uses(DatabaseMigrations::class);
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+});
+
+it('return all transactions', function () {
+
+    Income::factory()
+        ->for($this->user)
+        ->count(2)
+        ->create();
+
+    Expense::factory()
+        ->for($this->user)
+        ->count(3)
+        ->create();
+
+    actingAs($this->user)
+        ->getJson('api/transactions')
+        ->assertOk()
+        ->assertJsonCount(5, 'transactions');
+});
