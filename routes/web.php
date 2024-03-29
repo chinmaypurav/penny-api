3ca658b2cb380d275d2e67a9cfc72658df309c08
Chinmay Purav <chinpurav@gmail.com>
2024-03-29 23:07:20 +0530

    custom login routes deleted

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('login', [LoginController::class, 'login'])->middleware('guest');
-Route::post('logout', [LoginController::class, 'logout'])->middleware('auth');
